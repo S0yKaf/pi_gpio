8198176b512809b88c8469897aedc69ebca0938b
Kiniamaro <vdyotte@gmail.com>
2016-07-31 00:58:49 -0400

    fix goof

--- cloud_switch/cloud_switch.py
+++ cloud_switch/cloud_switch.py
@@ -13,10 +13,10 @@ toggle = False
 @app.route('/api/toggle', methods=['GET'])
 def toggle_switch():
     if toggle:
-        GPIO.output(app.config.SWITCH_PIN, GPIO.HIGH)
+        GPIO.output(app.config['SWITCH_PIN'], GPIO.HIGH)
         print('[Output] : HIGH')
     else:
-        GPIO.output(app.config.SWITCH_PIN, GPIO.LOW)
+        GPIO.output(app.config['SWITCH_PIN'], GPIO.LOW)
         print('[Output] : LOW')
     toggle = not toggle
 
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     app.config.from_pyfile('config/default_config.py')
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    GPIO.setup(app.config.SWITCH_PIN, GPIO.OUT)
+    GPIO.setup(app.config['SWITCH_PIN'], GPIO.OUT)
 
     if len(sys.argv) == 2:
         conf = sys.argv[1]
